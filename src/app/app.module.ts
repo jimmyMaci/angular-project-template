21ccc5a972328d7b382650a965def76f769ac637
jimmyMaci <tyler.durden69@gmx.de>
2020-03-12 12:05:54 +0100

    4 files modified, 7 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+    ])
+  ],
+  declarations: [ 
+    AppComponent, 
+    HelloComponent, 
+    ProductListComponent, 
+    TopBarComponent 
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
