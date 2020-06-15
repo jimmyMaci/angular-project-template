179aa91598486cbf370fbbe51992d8d2aafb277a
Asterios Raptis <net.asterios.raptis@mhp.com>
2020-06-15 11:07:24 +0200

    clean up from initial version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
 import {APP_BASE_HREF} from '@angular/common';
+import { APP_ROUTING } from "./app.routing";
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
-    ])
+    APP_ROUTING
   ],
   declarations: [ 
-    AppComponent, 
-    HelloComponent, 
-    ProductListComponent, 
-    TopBarComponent 
+    AppComponent,
   ],
   bootstrap:    [ AppComponent ],
   providers: [{provide: APP_BASE_HREF, useValue : '/' }]
