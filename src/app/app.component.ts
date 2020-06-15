538839f41d29829955feab3c6220c2b2172cea69
Asterios Raptis <net.asterios.raptis@mhp.com>
2020-06-15 12:54:27 +0200

    Update of angular version to 9.x

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "angular-app",
-  templateUrl: "./app.component.html",
-  styleUrls: [ "./app.component.css" ]
+  selector: 'app-angular',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent  {
-  name = "Angular";
+  name = 'Angular';
 }
