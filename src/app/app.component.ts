c37db5bc2072ee1848eedcfac05197bf3095b29d
b43054 <raptis.hays@swr.de>
2022-06-28 10:51:37 +0200

    upgrade of template with angular version 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: "angular-app",
   templateUrl: "./app.component.html",
   styleUrls: [ "./app.component.css" ]
 })
-export class AppComponent  {
-  name = "Angular";
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import {APP_BASE_HREF} from '@angular/common';
-import { APP_ROUTING } from "./app.routing";
+import {AppRoutingModule} from './app-routing.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    ReactiveFormsModule,
-    APP_ROUTING
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [ 
     AppComponent,
+    HomeComponent,
   ],
   bootstrap:    [ AppComponent ],
   providers: [{provide: APP_BASE_HREF, useValue : '/' }]
